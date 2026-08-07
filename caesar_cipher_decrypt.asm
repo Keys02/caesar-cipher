c90caa9f0374159a9cb8cab97d34aae803f68937
Keys <keys14052002@gmail.com>
2026-08-07 13:11:27 +0000

    chore: add pre-comments to the decryption program

--- caesar_cipher_decrypt.asm
+++ caesar_cipher_decrypt.asm
@@ -1,3 +1,26 @@
+;   Executable name : caesarcipherdecrypt
+;   Version         : 1.0
+;   Created date    : Mon, 07/08/2026
+;   Last Update     : Mon, 07/08/2026
+;   Author          : Opoku N. Chris
+;   Description     : An assembly language program that decrypts text using Caesar
+;                     Cipher algorithm
+;
+;
+; Build using these commands:
+;   nasm -­f elf64 -­g -­ F stabs caesarcipherdecrypt.asm
+;   ld -­ o caesarcipherdecrypt caesarcipherdecrypt.o
+;               or
+;   Using SASM editor build and save the the program as an exe file
+;
+; Running the program
+;   exefilename < input file > output file
+;               or
+;   echo "[Message]" | exefilename
+;   to run the program
+;
+;   If an output file is not specified, output goes to stdout
+
 section .data                       ; Section for initialized data 
     StatMsg: db "Decrypting...",0Ah
     StatLen: equ $-StatMsg
